904d706816290ba082fa832160ba4332e4788f3b
Alam Hosen <alamhosen.web@gmail.com>
2022-07-31 23:53:58 +0600

    mens fashion section added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Shared/Banner';
 import Gallery from './Gallery/Gallery';
+import MensFashion from './Mens_Fashion/MensFashion';
 import WomensFashion from './Womens_Fashion/WomensFashion';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Gallery></Gallery>
             <WomensFashion></WomensFashion>
+            <MensFashion></MensFashion>
 
         </div>
     );
---
+++ src/components/Home/Mens_Fashion/MensFashion.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import MensProduct from './MensProduct';
+
+const MensFashion = () => {
+    const [mensFashion, setmensFashion] = useState([])
+
+    useEffect( () => {
+        fetch('mensFashion.json')
+        .then(res => res.json())
+        .then(data => setmensFashion(data))
+    }, [])
+
+    return (
+        <div className='mb-28'>
+            <div className='text-center'>
+                <h1 className='font-bold text-xl mb-4'>MEN’S FASHION</h1>
+                <p className='text-base'>Shop our new arrivals from established brands</p>
+            </div>
+            <div className='flex items-center justify-center'>
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+
+                    {
+                        mensFashion.map(product => <MensProduct
+                            key={product.id}
+                            product={product}
+                        ></MensProduct>)
+                    }
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MensFashion;
